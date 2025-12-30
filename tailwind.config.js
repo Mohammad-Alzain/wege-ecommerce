187ac3f6e63067625dd975c3e356fb2aabacfbd1
mohammad-alzain <mohammadalzainsy99@gmail.com>
2025-12-30 12:31:34 +0300

    fix tailwindcss bug no config file with nextjs 16

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./modules/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E11D48", // example
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
